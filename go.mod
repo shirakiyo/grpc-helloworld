124e28d6f8f10dd93a6703fd95b3b39a0d463ec1
shirakiyo <tus.sk5678@gmail.com>
2020-04-16 01:31:16 +0900

    fix

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/shirakiyo/grpc-helloworld
+
+go 1.13
+
+require (
+	github.com/golang/mock v1.4.3
+	github.com/golang/protobuf v1.4.0
+	google.golang.org/grpc v1.28.1
+	google.golang.org/protobuf v1.21.0
+)
--- greeter_client/main.go
+++ greeter_client/main.go
@@ -26,7 +26,7 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "github.com/shirakiyo/grpc-helloworld/helloworld"
 )
 
 const (
@@ -51,6 +51,12 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+
+	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
